5d2eadd55c8dc61d04043d0f32f69b257bd23e7e
Eugene Sun <eug.sun@gmail.com>
2017-04-18 18:41:42 -0400

    Try to stay out of the way.

--- main.sh
+++ main.sh
@@ -1,13 +1,12 @@
 function run_if_exists {
     if [ -f $1 ]; then
-        echo "Running $1"
         . $1
     else
         echo "Can't find $1"
     fi
 }
 
-run_if_exists ~/dotfiles/build.sh
-run_if_exists ~/dotfiles/aliases.sh
-run_if_exists ~/dotfiles/funcs.sh
-run_if_exists ~/werkdotfiles/eugene/main.sh
+run_if_exists ~/.dotfiles/build.sh
+run_if_exists ~/.dotfiles/aliases.sh
+run_if_exists ~/.dotfiles/funcs.sh
+run_if_exists ~/.werkdotfiles/eugene/main.sh
