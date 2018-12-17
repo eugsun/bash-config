960a16f4b24d4d53f0cc61d337d58c67d95a6ad0
Eugene Y. Sun <eug.sun@gmail.com>
2018-12-17 16:42:45 -0500

    Clean up.

--- main.sh
+++ main.sh
@@ -1,12 +1,8 @@
 function run_if_exists {
     if [ -f $1 ]; then
         . $1
-    else
-        echo "Can't find $1"
     fi
 }
-
-if [[ -o login ]] cat ~/.dotfiles/startup.txt
 
 run_if_exists ~/.dotfiles/paths.sh
 run_if_exists ~/.dotfiles/aliases.sh
--- paths.sh
+++ paths.sh
@@ -9,3 +9,8 @@ run_if_exists $HOME/miniconda3/etc/profile.d/conda.sh
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
 run_if_exists $HOME/.rvm/scripts/rvm
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
